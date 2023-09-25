26b0f4ca9b3ce46aeebb4194b171ccdcc8499ccf
kaykyantonio <145937967+kaykyantonio@users.noreply.github.com>
2023-09-25 13:01:51 -0300

    Add files via upload
    botão arrumar calculadora adicionado, para caso dê problema com o tamanho por conta da quantia de caracteres existentes, agora eles são apagados e o tamanho da
    calculadora volta ao normal

---
+++ calculadora1/index.js
@@ -0,0 +1,22 @@
+function insertNumbers(number) {
+    let insert = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = insert + number;
+} function insertSymbols(symbols) {
+    let insertSymbols = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = insertSymbols + symbols;
+} function clearData() {
+    let clear = document.getElementById('resultado').innerHTML = '';
+} function calculate() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        alert('ERROR')
+    }
+}
+
+function corrigir_comprimento_calculadora(){
+    alert('arrumando calculadora..')
+    clearData()
+}
